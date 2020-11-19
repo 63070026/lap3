a812cb660634c9640c8f3b14fd936fa34b374b45
63070026 <73680622+63070026@users.noreply.github.com>
2020-11-19 21:00:58 +0700

    Add files via upload

--- lap3.php
+++ lap3.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'lap3.mysql.database.azure.com', 'Chayanon@lap3', 'Nikk05102001', 'itflab', 3306);
+mysqli_real_connect($conn, 'lap3-2.mysql.database.azure.com', 'Chayanon@lap3', 'Nikk05102001', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
